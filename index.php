1886b92a6855b80925d0d98c28a7a3c7e7aff204
Ruben Castillo <rcastillo@raivtech.com>
2020-08-18 21:10:06 -0600

    Index

---
+++ index.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- User styles -->
+    <link rel="stylesheet" href="css/styles.css">
+
+
+    <title>Yannelle Store</title>
+  </head>
+  <body>
+
+  <div class="container">
+    <h1 class="text-center">Yannelle</h1>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top bg-white shadow-sm rounded">
+  <div class="container">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav m-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown">
+            <a class="nav-link dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">
+              TECH
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <button class="dropdown-item" type="button"><i class="fab fa-apple"></i> Apple</button>
+              <button class="dropdown-item" type="button"><i class="fab fa-amazon"></i> Amazon</button>
+              <button class="dropdown-item" type="button"><i class="fab fa-google"></i> Google</button>
+              <button class="dropdown-item" type="button"><i class="fab fa-youtube"></i> YouTube</button>
+              <button class="dropdown-item" type="button"><i class="fab fa-facebook-square"></i> Facebook</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-code"></i> Code</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <div class="dropdown">
+            <a class="nav-link dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">
+              ENTRETENIMIENTO 
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <button class="dropdown-item" type="button"><i class="fas fa-film"></i> Peliculas</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-tv"></i> TV</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-gamepad"></i> Videojuegos</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-book"></i> Libros</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-music"></i> Musica</button>
+              <button class="dropdown-item" type="button"><i class="fas fa-podcast"></i> Podcast</button>
+            </div>
+          </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">NOSOTROS</a>
+        </li>
+    </ul>
+  </div>
+  </div>
+</nav> 
+<!-- fin navegacion -->
+
+<div class="container mt-4 lineBottom">
+  <div class="row">
+    <div class="col-md-6">
+      <h2>We have the best products for you.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorem nam, vero placeat tempore assumenda doloribus enim minus, omnis eum maiores quibusdam ut! Temporibus, minima. Numquam sed quam id ipsum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, exercitationem ut reprehenderit enim officia animi culpa repellendus non ab ipsum eligendi, delectus incidunt maxime eos debitis fugiat aliquam dolorem ratione. 
+      </p>
+
+      <button class="btn btn-outline-info btn-purchase">Purchase our products</button>
+    </div>
+    <div class="col-md-6">
+      <img src="images/sales.png" class="w-100"/>
+    </div>
+  </div>
+</div> <!-- first section -->
+
+<div class="container mt-5 mb-5 lineBottom" >
+  <div class="row mb-5">
+
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</div>
+
+
+
+<div class="container mt-5 lineBottom">
+  <div class="row">
+    <div class="col-md-6">
+      <img src="images/tech.png" class="w-100"/>
+    </div> <!-- fin col-md-6 -->
+    <div class="col-md-6">
+      <h2>Build websites with webify. Any niche. In minutes.</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem reiciendis dolore facere? Asperiores, explicabo reiciendis ducimus aliquam error dolores. Ut earum quas, alias autem repudiandae laudantium. Beatae ea inventore earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat illo ducimus inventore sit, iure atque unde nostrum autem voluptatem, enim temporibus nulla nesciunt deserunt id quidem fugiat veniam at corrupti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam cumque nemo quis maxime hic provident assumenda odio. Numquam quo molestiae magnam asperiores in nihil beatae commodi facilis et ad!
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">
+      </div>
+      <div class="swiper-slide">
+        <img src="images/sales.png" alt="">0</div>
+    </div>
+    <div class="swiper-pagination"></div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="js/swiper.js"></script>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ notas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.4/trix.min.css" integrity="sha512-sC2S9lQxuqpjeJeom8VeDu/jUJrVfJM7pJJVuH9bqrZZYqGe7VhTASUb3doXVk6WtjD0O4DTS+xBx2Zpr1vRvg==" crossorigin="anonymous" />
+    <title>Document</title>
+
+</head>
+<body>
+    
+<div class="form-group">
+    <label for="nota">Nota</label>
+    <input type="hidden" id="nota" name="nota" >
+    <trix-editor class="trix-content" input="nota"></trix-editor>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.4/trix.js" integrity="sha512-zEL66hBfEMpJUz7lHU3mGoOg12801oJbAfye4mqHxAbI0TTyTePOOb2GFBCsyrKI05UftK2yR5qqfSh+tDRr4Q==" crossorigin="anonymous"></script>
+</body>
+</html>
