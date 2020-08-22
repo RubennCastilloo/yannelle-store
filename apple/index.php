8af8d6c43d8b0b8d2d525c0fc166291d5e6deb28
Ruben Castillo <rcastillo@raivtech.com>
2020-08-22 12:29:41 -0600

    Diseño de página principal y subpáginas

---
+++ apple/index.php
@@ -0,0 +1,50 @@
+<?php 
+    include '../layout-sub/header.php';
+?>
+
+<div class="container mt-5 lineBottom">
+    <h2 class="text-center"><i class="fab fa-apple"></i> Apple</h2>
+    <p class="text-left mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium reiciendis cupiditate minima unde praesentium, inventore molestias aut aspernatur quo, molestiae, rem quae ipsum incidunt labore delectus cum dolor beatae repudiandae?. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veritatis omnis dicta fugiat nam eveniet labore dolores, neque voluptates tempora ut quis corrupti libero aliquam ea vel quisquam accusantium voluptas?</p>
+</div>
+
+<div class="container notes mt-5 lineBottom">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="row">
+                        <div class="col-md-4 col-sm-4 mb-4">
+                            <img src="../images/notes/netflix.jpg" alt="" class="h-200 w-100">
+                        </div>
+                        <div class="col-md-8 col-sm-8 lineBottom mb-4">
+                            <a class="links" href=""><h3>Netflix is testing a ‘Shuffle’ button, because you’re tired of picking what to watch</h3></a>
+                            <p>Por <a class="autorLink" href="">Ruben Castillo</a> | Agosto 21 | 12 Comentarios</p>
+                        </div>
+
+                        <div class="col-md-4 col-sm-4 mb-4">
+                            <img src="../images/notes/camera.jpg" alt="" class="h-200 w-100">
+                        </div>
+                        <div class="col-md-8 col-sm-8 lineBottom mb-4">
+                            <a class="links" href=""><h3>It’s time to watch all the Ip Man movies</h3></a>
+                            <p>Por <a class="autorLink" href="">Ruben Castillo</a> | Agosto 21 | 12 Comentarios</p>
+                        </div>
+
+                        <div class="col-md-4 col-sm-4 mb-4">
+                            <img src="../images/notes/netflix.jpg" alt="" class="h-200 w-100">
+                        </div>
+                        <div class="col-md-8 col-sm-8 lineBottom mb-4">
+                            <a class="links" href=""><h3>It’s time to watch all the Ip Man movies</h3></a>
+                            <p>Por <a class="autorLink" href="">Ruben Castillo</a> | Agosto 21 | 12 Comentarios</p>
+                        </div>
+
+
+                        
+            </div>
+        </div>
+        <div class="col-md-3 text-center">
+            Publicidad
+        </div>
+    </div>
+</div>
+
+<?php 
+    include '../layout-sub/footer.php';
+?>
--- index.php
+++ index.php
@@ -1,82 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
-
-
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <!-- User styles -->
-    <link rel="stylesheet" href="css/styles.css">
-
-
-    <title>Yannelle Store</title>
-  </head>
-  <body>
-
-  <div class="container">
-    <h1 class="text-center">Yannelle</h1>
-  </div>
-  <nav class="navbar navbar-expand-lg navbar-light sticky-top bg-white shadow-sm rounded">
-  <div class="container">
-  
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav m-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <div class="dropdown">
-            <a class="nav-link dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">
-              TECH
-            </a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-              <button class="dropdown-item" type="button"><i class="fab fa-apple"></i> Apple</button>
-              <button class="dropdown-item" type="button"><i class="fab fa-amazon"></i> Amazon</button>
-              <button class="dropdown-item" type="button"><i class="fab fa-google"></i> Google</button>
-              <button class="dropdown-item" type="button"><i class="fab fa-youtube"></i> YouTube</button>
-              <button class="dropdown-item" type="button"><i class="fab fa-facebook-square"></i> Facebook</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-code"></i> Code</button>
-            </div>
-          </div>
-        </li>
-
-        <li class="nav-item">
-          <div class="dropdown">
-            <a class="nav-link dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">
-              ENTRETENIMIENTO 
-            </a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-              <button class="dropdown-item" type="button"><i class="fas fa-film"></i> Peliculas</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-tv"></i> TV</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-gamepad"></i> Videojuegos</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-book"></i> Libros</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-music"></i> Musica</button>
-              <button class="dropdown-item" type="button"><i class="fas fa-podcast"></i> Podcast</button>
-            </div>
-          </div>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="#">NOSOTROS</a>
-        </li>
-    </ul>
-  </div>
-  </div>
-</nav> 
-<!-- fin navegacion -->
+<?php 
+  include './layout/header.php';
+?>
 
 <div class="container mt-4 lineBottom">
   <div class="row">
@@ -97,18 +21,22 @@
   </div>
 </div> <!-- first section -->
 
-<div class="container mt-5 mb-5 lineBottom" >
-  <div class="row mb-5">
 
+<div class="container mt-5 mb-5 lineBottom" >
+  <div class="text-uppercase text-center mb-5">
+    <small>Ultimas</small>
+    <h3>noticias</h3>
+  </div>
+
+  <div class="row mb-5">
   <div class="col-md-4">
     <div class="card mb-4 shadow-sm">
       <img class="card-img-top" src="images/sales.png" alt="Card image cap">
       <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          </div>
         </div>
       </div>
     </div>
@@ -118,11 +46,10 @@
     <div class="card mb-4 shadow-sm">
       <img class="card-img-top" src="images/sales.png" alt="Card image cap">
       <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          </div>
         </div>
       </div>
     </div>
@@ -132,16 +59,55 @@
     <div class="card mb-4 shadow-sm">
       <img class="card-img-top" src="images/sales.png" alt="Card image cap">
       <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          </div>
         </div>
       </div>
     </div>
   </div>
 
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img class="card-img-top" src="images/sales.png" alt="Card image cap">
+      <div class="card-body">
+      <span class="badge badge-light">Apple</span>
+      <a href="#" class="card-link"><h3>Titulo de la nota</h3></a>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="d-flex justify-content-between align-items-center">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
   </div>
 </div>
 
@@ -164,7 +130,7 @@
   </div>
 </div>
 
-<div class="swiper-container">
+<!-- <div class="swiper-container">
     <div class="swiper-wrapper">
       <div class="swiper-slide">
         <img src="images/sales.png" alt="">
@@ -198,22 +164,10 @@
     </div>
     <div class="swiper-pagination"></div>
     <!-- Add Arrows -->
-    <div class="swiper-button-next"></div>
+    <!-- <div class="swiper-button-next"></div>
     <div class="swiper-button-prev"></div>
-  </div>
+  </div> -->
 
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="js/swiper.js"></script>
-
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
-</html>
+  <?php 
+    include './layout/footer.php';
+  ?>
